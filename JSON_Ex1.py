e09085b99fec48edd1eb090947d333a11924e3e9
beatricx <72341578+beatricexc@users.noreply.github.com>
2022-05-07 14:51:21 +0100

    Update JSON_Ex1.py

--- JSON_Ex1.py
+++ JSON_Ex1.py
@@ -1,1 +1,15 @@
+import json
 
+data = {
+     "user": { 
+       "name": "Jane Janet",
+       "age" : 49
+       
+     }      
+}
+
+with open ("data_file.json", "w") as write_file:
+  json.dump(data, write_file)
+  
+json_str = jdson.dumps(data)
+print(json_str)
